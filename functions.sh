6ac28183b566895aaca7ea1b7aca7dea3d265479
Alexey Ignatov <lexszero@gmail.com>
2016-01-28 13:08:37 +0300

    Infrastructure for module hooks, helper functions, cleanups

--- functions.sh
+++ functions.sh
@@ -1,23 +1,216 @@
 #!/bin/bash
 CONFIG="/etc/wb-hardware.conf"
-DATADIR="${DATADIR:-/usr/share/wb-hwconf-manager}"
+DATADIR="/usr/share/wb-hwconf-manager"
+CONFIG_STATE="/var/lib/wirenboard/hardware.state"
+
+DEBUG=""
+SYSLOG=""
+
+VERBOSE="yes"
+2>/dev/null . /lib/lsb/init-functions
+
+2>/dev/null . ./debug-env
+
 MODULES="$DATADIR/modules"
 SLOTS="$DATADIR/slots"
 
 CONFIGFS="/sys/kernel/config"
 OVERLAYS="$CONFIGFS/device-tree/overlays"
 
-VERBOSE="yes"
-. /lib/lsb/init-functions 2>/dev/null
+debug() { :; }
+if [[ -n "$DEBUG" ]]; then
+	unset debug
+	debug() {
+		>&2 echo -e "[${FUNCNAME[2]}]$([[ -n "$SLOT" || -n "$MODULE" ]] && echo " $SLOT:$MODULE"): $*"
+	}
+fi
 
 die() {
-	#>&2 echo "[${FUNCNAME[1]}] ERROR: $@"
-	log_failure_msg "$@"
-	#[[ $? == 0 ]] && exit 1 || exit $?
+	debug "ERROR: $*"
+	if [[ -n "$SYSLOG" ]]; then
+		logger -p user.err -t "wb-hwconf-manager" "$*"
+	else
+		log_failure_msg "$*"
+	fi
+	[[ $? == 0 ]] && return 1 || return $?
 }
 
-overlay_path() {
-	echo "$OVERLAYS/$1:$2"
+log() {
+	if [[ -n "$SYSLOG" ]]; then
+		logger -p user.info -t "wb-hwconf-manager" "$*"
+	else
+		log_failure_msg "$*"
+	fi
+}
+
+# Join array to string
+# Args:
+# - delimiter
+# - rest args are the array items
+# Example: `join , 1 2 3` == "1,2,3"
+join() {
+	local IFS="$1"
+	shift
+	echo "$*"
+}
+
+################################################################################
+# JSON handling functions
+#
+# It's expected that $JSON variable will contain the name of json file 
+# that is to be processed
+################################################################################
+
+# Runs jq with given arguments and replaces the original file with result
+# Example: json_edit '.foo = 123'
+json_edit() {
+	[[ -e "$JSON" ]] || {
+		die "JSON file '$JSON' not found"
+		return 1
+	}
+
+	local tmp=`mktemp`
+	sed 's#//.*##' "$JSON" |	# there are // comments, strip them out
+	jq "$@" > "$tmp"
+	cat "$tmp" > "$JSON"
+	rm "$tmp"
+	debug "json_edit\n"
+	>&2 cat "$JSON"
+}
+
+# Find item in array.
+# Example: json_array_find '.slots' '.id == "foo"'
+json_array_find() {
+	[[ -e "$JSON" ]] || {
+		die "JSON file '$JSON' not found"
+		return 1
+	}
+
+	jq -e "${1}[] | select($2)" "$JSON"
+}
+
+# Append items to array
+# Example: json_array_append '.slots' '{id: "foo", type: "bar", name: "baz"}'
+json_array_append() {
+	local array=$1
+	shift
+	json_edit "${array} = [${array}[], $(join ", " "$@")]"
+}
+
+# Delete matching array items
+# Example: json_array_delete '.slots' '.id == "foo"'
+json_array_delete() {
+	json_edit "${1} = (${1} | map(select(($2) | not)))"
+}
+
+# Update matching array items
+# Example: json_array_update '.slots' '.id == "foo"' '.module = "bar"'
+#	this will set .module to "bar" for array items having .id == "foo"
+json_array_update() {
+	json_edit "${1} = (${1} | map(if ($2) then ($3) else . end))"
+}
+
+################################################################################
+# Config handling functions
+################################################################################
+
+config_parse() {
+	jq -r '.slots[] | @text "\(.id) \(.module)"' "$CONFIG"
+}
+
+config_slot_module() {
+	jq -r ".slots[] | select(.id == \"$1\") | .module" "$CONFIG"
+}
+
+config_slot_add() {
+	local SLOT=$1
+	local JSON=$CONFIG
+
+	slot_get_filename >/dev/null || return 1
+
+	json_array_find ".slots" ".id == \"$SLOT\"" >/dev/null && {
+		die "Slot $SLOT already present in config"
+		return 100
+	}
+	
+	log "Adding slot $SLOT"
+	json_array_append ".slots" \
+		"{id: \"$1\", type: \"$2\", name: \"$3\", module: \"\"}"
+}
+
+config_slot_del() {
+	local SLOT=$1
+	local JSON=$CONFIG
+
+	json_array_find >/dev/null ".slots" ".id == \"$SLOT\"" || {
+		die "Slot $SLOT not present in config"
+		return 100
+	}
+	
+	local m=$(config_slot_module "$SLOT")
+	[[ -n "$m" ]] && {
+		die "Slot $SLOT is used by module $m, remove it first"
+		return 101
+	}
+
+	log "Deleting slot $SLOT"
+	json_array_delete ".slots" ".id == \"$SLOT\""
+}
+
+slot_get_filename() {
+	local slot=${1:-$SLOT}
+	[[ -z "$slot" ]] && {
+		die "SLOT is unset"
+		return 1
+	}
+
+	slot="$SLOTS/${slot}.def"
+	[[ -f "$slot" ]] || {
+		die "Slot definition file ${slot} not found"
+		return 1
+	}
+
+	echo "$slot"
+}
+
+module_get_filename() {
+	local mod=${1:-$MODULE}
+	[[ -z "$mod" ]] && {
+		die "MODULE is unset"
+		return 1
+	}
+	local ext=${2:-dtso}
+
+	mod="$MODULES/${mod}.${ext}"
+	[[ -f "$mod" ]] || {
+		die "Module definition file ${mod} not found"
+		return 1
+	}
+
+	echo "$mod"
+}
+
+slot_get_vars() {
+	SLOT=${SLOT:-$1} slot_preprocess -DFROM_SHELL <<<"" | sed 's/#.*//; /^$/d'
+}
+
+slot_preprocess() {
+	local slot=`slot_get_filename` || return 1
+
+	cat "$slot" - | tcc \
+		-x assembler-with-cpp \
+		-nostdinc \
+		-I "$SLOTS" -I "$MODULES" "$@" -E -
+}
+
+dtbo_build() {
+	local mod=`module_get_filename` || return 1
+
+	{
+		echo '/dts-v1/ /plugin/;'
+		cat "$mod"
+	} |
+	slot_preprocess #| dtc -I dts -O dtb -@ -
 }
 
 dtbo_check_compatible() {
@@ -29,46 +222,43 @@ dtbo_check_compatible() {
 	return 1
 }
 
-dtbo_build() {
-	local _slot=$1
-	local _mod=$2
-	[[ -z "$_slot" || -z "$_mod" ]] && {
-		die "Bad arguments"
-		return 1
+module_run_hook() {
+	local func=hook_module_${1}
+	local file="$MODULES/${MODULE}.sh"
+
+	module_get_filename >/dev/null || return 1
+
+	[[ -e "$file" ]] || {
+		debug "No hooks script file"
+		return 0
 	}
 
-	local slot="$SLOTS/${_slot}.def"
-	[[ -f "$slot" ]] || {
-		die "Slot definition file ${slot} not found"
-		return 1
+	unset $func
+	source $file
+	local t=`type -t $func`
+	[[ -n "$t" && "$t" == function ]] || {
+		debug "Hook function $func is not defined"
+		return 0
 	}
 
-	local mod="$MODULES/${_mod}.dtso"
-	[[ -f "$mod" ]] || {
-		die "Module definition file ${mod} not found"
-		return 1
-	}
+	$func
+	unset $func
+}
 
-	{
-		echo '/dts-v1/ /plugin/;'
-		cat "$slot" "$mod"
-	} | tcc \
-		-x assembler-with-cpp \
-		-nostdinc \
-		-I "$SLOTS" -I "$MODULES" -E - |
-	dtc -I dts -O dtb -@ -
+overlay_path() {
+	echo "$OVERLAYS/$1:$2"
 }
 
 module_bind() {
-	local _slot=$1
-	local _mod=$2
+	local SLOT=$1
+	local MODULE=$2
 
-	local t=`echo "$OVERLAYS/$_slot:"*`
+	local t=`echo "$OVERLAYS/$SLOT:"*`
 	[[ -d "$t" ]] && {
 		local st="$(cat "$t/status")"
 		case "$st" in
 			"applied")
-				die "Slot $_slot is used by ${t#$OVERLAYS/$_slot:} module"
+				die "Slot $SLOT is used by ${t#$OVERLAYS/$SLOT:} module"
 				return 1
 				;;
 			*)
@@ -77,23 +267,27 @@ module_bind() {
 				;;
 		esac
 	}
-	t=`echo "$OVERLAYS/$_slot:"*`
+	t=`echo "$OVERLAYS/$SLOT:"*`
 	[[ -d "$t" ]] && {
-		die "Overlays conflict on slot $_slot"
+		die "Overlays conflict on slot $SLOT"
 		return 1
 	}
 
-	log_action_msg "Binding module $_mod to slot $_slot"
+	log_action_msg "Binding module $MODULE to slot $SLOT"
 
 	local dtbo=`mktemp`
-	dtbo_build $_slot $_mod > "$dtbo"
+	dtbo_build > "$dtbo" || {
+		rm "$dtbo"
+		die "Device Tree overlay building failed"
+		return 1
+	}
 	dtbo_check_compatible "$dtbo" || {
 		rm "$dtbo"
 		die "Device Tree overlay is incompatible with this device"
 		return 1
 	}
 
-	local overlay=`overlay_path $_slot $_mod`
+	local overlay=`overlay_path $SLOT $MODULE`
 	mkdir "$overlay" &&
 	cat "$dtbo" > "$overlay/dtbo"
 
@@ -108,22 +302,22 @@ module_bind() {
 		die "Device Tree overlay loading failed"
 		return 1
 	}
-
 	rm "$dtbo"
+
+	module_run_hook init
 }
 
 module_unbind() {
-	local _slot=$1
+	local SLOT=$1
 
-	log_action_msg "Unbinding module from slot $_slot"
-	local t=`echo "$OVERLAYS/$_slot:"*`
+	log_action_msg "Unbinding module from slot $SLOT"
+	local t=`echo "$OVERLAYS/$SLOT:"*`
 	[[ -e "$t" ]] || {
-		log_end_msg "Slot $_slot is not in use"
+		log_end_msg "Slot $SLOT is not in use"
 	}
 
+	module_run_hook deinit
 	rmdir "$t"
 }
 
-config_parse() {
-	jq -r '.slots[] | @text "\(.id) \(.module)"' "$CONFIG"
-}
+
---
+++ modules/utils.sh
@@ -0,0 +1,43 @@
+eval `slot_get_vars $SLOT`
+local SLOT_TYPE=${SLOT%%[0-9]}
+local SLOT_NUM=${SLOT#$SLOT_TYPE}
+
+CONFIG_GPIO=${CONFIG_GPIO:-/etc/wb-homa-gpio.conf}
+
+# Add GPIO to the wb-homa-gpio driver config
+# Args:
+# - gpio name (for mqtt)
+# - gpio number (as in /sys/class/gpio)
+# - direction - "input" or "output"
+# These 3 args can be repeated multiple times to add many gpios at once
+wb_gpio_add() {
+	local JSON=$CONFIG_GPIO
+	local items=()
+	while [[ $# -ge 3 ]]; do
+		[[ -n "$1" && -n "$2" && -n "$3" ]] || {
+			die "Bad arguments"
+			return 1
+		}
+		items+=( "{name: \"$1\", gpio: $2, direction: \"$3\"}" )
+		shift 3
+	done
+
+	json_array_append ".channels" "${items[@]}"
+}
+
+# Remove GPIO from the wb-homa-gpio driver config
+# Args:
+# - gpio number (as in /sys/class/gpio)
+wb_gpio_del() {
+	local JSON=$CONFIG_GPIO
+	json_array_delete ".channels" \
+		". as \$chan | ([$(join ', ' "$@")] | any(. == \$chan.gpio))"
+}
+
+wb_max_slot_num() {
+	jq '
+		[ .slots[].id | select(test("wb5-extio[0-9]+$")) ] |
+		map(match("[0-9]+$") | .string | tonumber) |
+		max
+	' "$CONFIG"
+}
