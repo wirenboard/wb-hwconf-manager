86c0f299b864a2bb739d6f33fa8093be97612ca0
Alexey Ignatov <lexszero@gmail.com>
2016-02-02 22:19:24 +0300

    modules: wbe-di-dr-3: Fix added GPIO naming

--- modules/wbe-di-dr-3.sh
+++ modules/wbe-di-dr-3.sh
@@ -1,9 +1,9 @@
 source "$DATADIR/modules/utils.sh"
 
 hook_module_add() {
-	wb_gpio_add "MOD${sn}_IN1" $GPIO_SDA input
-	wb_gpio_add "MOD${sn}_IN2" $GPIO_SCL input
-	wb_gpio_add "MOD${sn}_IN3" $GPIO_CS input
+	wb_gpio_add "MOD${SLOT_NUM}_IN1" $GPIO_SDA input
+	wb_gpio_add "MOD${SLOT_NUM}_IN2" $GPIO_SCL input
+	wb_gpio_add "MOD${SLOT_NUM}_IN3" $GPIO_CS input
 }
 
 hook_module_del() {
