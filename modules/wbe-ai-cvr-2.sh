053024d4b82a2dba667a1e6a4d317e4c11d33af2
Alexey Ignatov <lexszero@gmail.com>
2016-03-25 21:53:50 +0300

    modules: wbe-ai-cvr-2: Add init/deinit hooks to manually add/del ads1015 i2c slave

--- modules/wbe-ai-cvr-2.sh
+++ modules/wbe-ai-cvr-2.sh
@@ -7,3 +7,21 @@ hook_module_add() {
 hook_module_del() {
 	hook_once_after_config_change "service_restart_delete_retained wb-homa-adc '/devices/wb-adc/#'"
 }
+
+hook_module_init() {
+	local bus=$(slot_i2c_bus_sysfs)
+	[[ -d "$bus" ]] || {
+		log "Unable to find i2c bus for slot $SLOT (sysfs: $bus)"
+		return 1
+	}
+	echo 'ads1015 0x48' > $bus/new_device
+}
+
+hook_module_deinit() {
+	local bus=$(slot_i2c_bus_sysfs)
+	[[ -d "$bus" ]] || {
+		log "Unable to find i2c bus for slot $SLOT (sysfs: $bus)"
+		return 1
+	}
+	echo '0x48' > $bus/delete_device
+}
