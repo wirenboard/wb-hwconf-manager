c2ff9733f44b73ad0bb35f19df1bce0bb02e890d
Evgeny Boger <boger@contactless.ru>
2016-04-05 20:43:44 +0300

    fix netmon module

--- modules/wbe-netmon21.sh
+++ modules/wbe-netmon21.sh
@@ -4,7 +4,7 @@ hook_module_init() {
     echo $GPIO_RX > /sys/class/gpio/export
     echo low > /sys/class/gpio/gpio${GPIO_RX}/direction
 
-    local BUS_NUM=`grep -l '^${SLOT_ALIAS}@0$' /sys/bus/i2c/devices/i2c-*/name  | grep -Po '(?<=i2c-)(\d+)'`
+    local BUS_NUM=`grep -l "^${SLOT_ALIAS}_i2c@0$" /sys/bus/i2c/devices/i2c-*/name  | grep -Po '(?<=i2c-)(\d+)'`
 
     local JSON="/etc/wb-mqtt-am2320.conf"
     json_edit ".i2c_bus = ${BUS_NUM}"
