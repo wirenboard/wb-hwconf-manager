58eaac463c72c1fc9162955a97b4513e4f2d72c3
Evgeny Boger <boger@contactless.ru>
2016-04-21 10:58:59 +0300

    adds support for relay extension module

---
+++ modules/wbe-do-r6c-1.sh
@@ -0,0 +1,11 @@
+source "$DATADIR/modules/utils.sh"
+
+hook_module_add() {
+	wb_gpio_add "MOD${SLOT_NUM}_OUT1" $GPIO_CS output
+	hook_once_after_config_change "service_restart_delete_retained wb-homa-gpio /devices/wb-gpio/#"
+}
+
+hook_module_del() {
+	wb_gpio_del $GPIO_CS
+	hook_once_after_config_change "service_restart_delete_retained wb-homa-gpio /devices/wb-gpio/#"
+}
