9a24ca126069a003d053343d7298fcd2ed69a1fb
Evgeny Boger <boger@contactless.ru>
2016-11-17 16:08:04 +0300

    mod-rtc: change mod RTC behaviour: SoC RTC is not present in the system

--- modules/wb56-mod-rtc.sh
+++ modules/wb56-mod-rtc.sh
@@ -1,5 +1,21 @@
 source "$DATADIR/modules/utils.sh"
 
+set_hwclock_rtc_dev() {
+    # this may cause problems if there were commented-out HCTOSYS_DEVICE lines
+    sed -i.bak 's/^#*HCTOSYS_DEVICE=rtc./HCTOSYS_DEVICE=rtc'$1/ /etc/default/hwclock
+}
+
 hook_module_init() {
-	hwclock -s
+
+    # set time from RTC if only it looks reasanoble (e.g. > 2013-01-01)
+    HW_RTC_EPOCH=`cat /sys/class/rtc/rtc1/since_epoch`
+    if (( ${HW_RTC_EPOCH} > 1356998400 )); then
+	   hwclock -f /dev/rtc1 -s
+    fi;
+
+    set_hwclock_rtc_dev 1
+}
+
+hook_module_deinit() {
+    set_hwclock_rtc_dev 0
 }
