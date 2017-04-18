551da6b4075f27affa73469e6469a98148e0d749
Alexey Ignatov <lexszero@gmail.com>
2017-04-18 23:18:35 +0300

    gen_extio_slots.sh: remove redundant #include "wb-extio-v1.h"

--- slots/gen_extio_slots.sh
+++ slots/gen_extio_slots.sh
@@ -9,8 +9,6 @@ for board_inc in *-extio.h; do
 		slot=$[i+1]
 		cat > "${board_type}-extio${slot}.def" <<EOF
 #define EXTIO_SLOT_NUM ${slot}
-
-#include "wb-extio-v1.h"
 #include "${board_inc}"
 EOF
 	done
