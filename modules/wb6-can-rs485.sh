b31584b0efe1c73e17f6109c94215c10b01d61ae
Evgeny Boger <boger@contactless.ru>
2017-07-04 21:41:46 +0300

    wb6: CAN_OFF => CAN_EN

--- modules/wb6-can-rs485.sh
+++ modules/wb6-can-rs485.sh
@@ -1,11 +1,11 @@
 source "$DATADIR/modules/utils.sh"
 
 hook_module_init() {
-	sysfs_gpio_export $GPIO_CAN_OFF
-	sysfs_gpio_set $GPIO_CAN_OFF 1
+	sysfs_gpio_export $GPIO_CAN_EN
+	sysfs_gpio_set $GPIO_CAN_EN 1
 }
 
 hook_module_deinit() {
-	sysfs_gpio_direction $GPIO_CAN_OFF in
-	sysfs_gpio_unexport $GPIO_CAN_OFF
+	sysfs_gpio_direction $GPIO_CAN_EN in
+	sysfs_gpio_unexport $GPIO_CAN_EN
 }
