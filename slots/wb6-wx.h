b5b0fa23198605204211676a4e49e782cda5080c
Evgeny Boger <boger@contactless.ru>
2017-09-22 21:50:19 +0300

    wb6: allows to use W1 and W2 terminals as an input

---
+++ slots/wb6-wx.h
@@ -0,0 +1,8 @@
+/* Board-independent definitions for WB6 Wx 'slots' */
+
+/* All known pins for module */
+#define SLOT_ALL_PINS \
+	SLOT_FOR_PIN(DATA) \
+	SLOT_FOR_PIN(PULLUP)
+
+#include "utils.h"
