9a55fdfa5b397569c68af139ce3fca5a09672065
Nikita Kulikov <n.kulikov@wirenboard.ru>
2018-06-09 17:34:13 +0300

    Fixed work WBE2-I-AO10V-2 in MOD2 slot (i2c SDA PULLUP via JTAG_MOD pin) via overlay (use led driver) and simultaneously work of two modules.

--- modules/wbe2-ao-10v-2.sh
+++ modules/wbe2-ao-10v-2.sh
@@ -1,17 +1,16 @@
 source "$DATADIR/modules/utils.sh"
 local CONFIG_DAC=${CONFIG_DAC:-/etc/wb-mqtt-dac.conf}
+local IIO_OF_NAME="${SLOT_ALIAS}_wbe2_ao_10v_2"
 
-hook_module_init() {
-    local IIO_BUS_NUM=`ls -d /sys/devices/platform/${SLOT_ALIAS}_i2c@0/*/*/iio:device* | grep -Po '(?<=iio:device)(\d+)'`
-
+hook_module_add() {
     local JSON=$CONFIG_DAC
     local items=()
     local chan
     for chan in 0 1; do
         items+=( "{
             id: \"MOD${SLOT_NUM}_O$((chan+1))\",
-            iio_device: ${IIO_BUS_NUM},
             iio_channel: $chan,
+            iio_of_name: \"${IIO_OF_NAME}\",
             max_value_mv: 10000,
             multiplier: 3.75
         }" )
@@ -25,7 +24,6 @@ hook_module_init() {
 
 hook_module_del() {
     local JSON=$CONFIG_DAC
-    json_array_delete ".channels" \
-        ". as \$chan | ([\"MOD${SLOT_NUM}_O1\", \"MOD${SLOT_NUM}_O2\"] | map(. == \$chan.id) | any)"
+    json_array_delete ".channels" ".iio_of_name==\"${IIO_OF_NAME}\""
     hook_once_after_config_change "service_restart_delete_retained wb-rules /devices/wb-dac/#"
 }
