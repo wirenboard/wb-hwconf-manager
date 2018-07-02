33c9c69882761fff44c272e270f50aeea96bc4e3
Nikita Kulikov <nikitoz236@gmail.com>
2018-07-02 12:53:34 +0300

    fix default of bias disable signal

--- modules/wb6-rs485.sh
+++ modules/wb6-rs485.sh
@@ -1,15 +1,15 @@
 source "$DATADIR/modules/utils.sh"
 
 hook_module_add() {
-	sysfs_gpio_export $GPIO_CAN_EN
-	sysfs_gpio_set $GPIO_CAN_EN 1
+#	sysfs_gpio_export $GPIO_CAN_EN
+#	sysfs_gpio_set $GPIO_CAN_EN 1
 
 	sysfs_gpio_export $GPIO_RS485_FS
 
 	local bias_mode="$(config_module_option ".mode")"
-	if [ "$bias_mode" = "enabled" ]
+	if [ "$bias_mode" = "disabled" ]
 	then
-		echo "failsafe bias enabled"
+		echo "failsafe bias disabled"
 		sysfs_gpio_set $GPIO_RS485_FS 0
 	else
 		echo "failsafe bias enabled"
@@ -20,8 +20,8 @@ hook_module_add() {
 }
 
 hook_module_del() {
-	sysfs_gpio_direction $GPIO_CAN_EN in
-	sysfs_gpio_unexport $GPIO_CAN_EN
+#	sysfs_gpio_direction $GPIO_CAN_EN in
+#	sysfs_gpio_unexport $GPIO_CAN_EN
 
 	sysfs_gpio_direction $GPIO_RS485_FS in
 	sysfs_gpio_unexport $GPIO_RS485_FS
