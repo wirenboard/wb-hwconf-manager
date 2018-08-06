2124b20799ab2b76bc514dba1b748a6097f9ea31
Nikita Kulikov <nikitoz236@gmail.com>
2018-08-06 16:45:11 +0300

    try control can transiver via regulator

--- modules/wb6-rs485-can.sh
+++ modules/wb6-rs485-can.sh
@@ -6,6 +6,9 @@ hook_module_init() {
 	
 	sysfs_gpio_export $GPIO_RS485_RTS
 	sysfs_gpio_set $GPIO_RS485_RTS 0
+
+	sysfs_gpio_export $GPIO_RS485_FS
+	sysfs_gpio_set $GPIO_RS485_FS 0
 }
 
 hook_module_deinit() {
@@ -14,4 +17,7 @@ hook_module_deinit() {
 
 	sysfs_gpio_direction $GPIO_RS485_RTS in
 	sysfs_gpio_unexport $GPIO_RS485_RTS
+	
+	sysfs_gpio_direction $GPIO_RS485_FS in
+	sysfs_gpio_unexport $GPIO_RS485_FS
 }
