c02f285311990fd37a67b67501ff1701428a9f8c
Nikita Kulikov <nikitoz236@gmail.com>
2018-09-26 18:13:59 +0300

    fix control can tranciever via gpio

--- modules/wb6-rs485.sh
+++ modules/wb6-rs485.sh
@@ -1,8 +1,8 @@
 source "$DATADIR/modules/utils.sh"
 
 hook_module_add() {
-#	sysfs_gpio_export $GPIO_CAN_EN
-#	sysfs_gpio_set $GPIO_CAN_EN 1
+	sysfs_gpio_export $GPIO_CAN_EN
+	sysfs_gpio_set $GPIO_CAN_EN 1
 
 	sysfs_gpio_export $GPIO_RS485_FS
 
@@ -20,8 +20,8 @@ hook_module_add() {
 }
 
 hook_module_del() {
-#	sysfs_gpio_direction $GPIO_CAN_EN in
-#	sysfs_gpio_unexport $GPIO_CAN_EN
+	sysfs_gpio_direction $GPIO_CAN_EN in
+	sysfs_gpio_unexport $GPIO_CAN_EN
 
 	sysfs_gpio_direction $GPIO_RS485_FS in
 	sysfs_gpio_unexport $GPIO_RS485_FS
