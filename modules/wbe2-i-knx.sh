41fdaf9f893184e6a267ed03a901768c7eafa5ad
munrexio <munrexio@gmail.com>
2020-05-18 16:20:21 +0300

    Add knx symlink
    Создание симлинка /dev/ttyKNX при выборе модуля KNX.

--- modules/wbe2-i-knx.sh
+++ modules/wbe2-i-knx.sh
@@ -1,7 +1,17 @@
 source "$DATADIR/modules/utils.sh"
 
 hook_module_init() {
+	if [[ -e /dev/ttyKNX ]]; then
+		ln -s /dev/ttyMOD${SLOT_NUM} /dev/ttyKNX${SLOT_NUM}
+	else
+		ln -s /dev/ttyMOD${SLOT_NUM} /dev/ttyKNX
+	fi
 }
 
 hook_module_deinit() {
+	if [[ -e /dev/ttyKNX${SLOT_NUM} ]]; then
+		rm -f /dev/ttyKNX${SLOT_NUM}
+	else
+		rm -f /dev/ttyKNX
+	fi
 }
