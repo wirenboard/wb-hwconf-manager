58221421d7f88938f7bac3376ba4cca11284c15a
munrexio <munrexio@gmail.com>
2020-05-20 20:59:36 +0300

    Убрал код из двух одинаковых файлов в один
    Код для wb5 и wb6 был одинаковый. Убрал в третий файл, сделал ссылки на него.

--- modules/wbe-i-knx.sh
+++ modules/wbe-i-knx.sh
@@ -1,15 +1,1 @@
-source "$DATADIR/modules/utils.sh"
-
-hook_module_init() {
-	ln -s /dev/ttyMOD${SLOT_NUM} /dev/ttyKNX${SLOT_NUM}
-	if [[ ! -e /dev/ttyKNX ]]; then
-		ln -s /dev/ttyMOD${SLOT_NUM} /dev/ttyKNX	
-	fi	
-}
-
-hook_module_deinit() {
-	rm -f /dev/ttyKNX${SLOT_NUM}
-	if [[ $(readlink /dev/ttyKNX) == "/dev/ttyMOD${SLOT_NUM}" ]]; then
-		rm -f /dev/ttyKNX	
-	fi
-}
+source "$DATADIR/modules/wbex-i-knx.sh"
