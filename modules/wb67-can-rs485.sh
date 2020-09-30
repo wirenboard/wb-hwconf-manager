b7758f96af123f37b92ee61302c2a27f655a8e0c
vdromanov <vdromanov@yahoo.com>
2020-09-30 19:47:34 +0300

    wb6.7.x terminators toggling has pulled to webui

--- modules/wb67-can-rs485.sh
+++ modules/wb67-can-rs485.sh
@@ -4,6 +4,9 @@ hook_module_init() {
 	sysfs_gpio_export $GPIO_RS485_FS
 	sysfs_gpio_direction $GPIO_RS485_FS out
 
+	sysfs_gpio_export $GPIO_RS485_TERM
+	sysfs_gpio_direction $GPIO_RS485_TERM out
+
 	local bias_mode="$(config_module_option ".mode")"
 	if [ "$bias_mode" = "disabled" ]
 	then
@@ -13,9 +16,22 @@ hook_module_init() {
 		echo "failsafe bias enabled"
 		sysfs_gpio_set $GPIO_RS485_FS 1
 	fi
+
+	local term_mode="$(config_module_option ".terminatorsMode")"
+	if [ "$term_mode" = "disabled" ]
+	then
+		echo "terminators are disabled"
+		sysfs_gpio_set $GPIO_RS485_TERM 0
+	else
+		echo "terminators are enabled"
+		sysfs_gpio_set $GPIO_RS485_TERM 1
+	fi
 }
 
 hook_module_deinit() {
 	sysfs_gpio_direction $GPIO_RS485_FS in
 	sysfs_gpio_unexport $GPIO_RS485_FS
+
+	sysfs_gpio_direction $GPIO_RS485_TERM in
+	sysfs_gpio_unexport $GPIO_RS485_TERM
 }
