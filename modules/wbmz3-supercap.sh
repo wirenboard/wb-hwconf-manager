9996cfb2e457a9cb16f60331d63ff8593931d35e
vdromanov <vdromanov@yahoo.com>
2020-10-13 15:21:08 +0300

    discharge signal has inverted on wbmz3-supercap

--- modules/wbmz3-supercap.sh
+++ modules/wbmz3-supercap.sh
@@ -1,1 +1,7 @@
 source "$DATADIR/modules/wbmz-supercap-common.sh"
+
+hook_module_add() {
+	wb_gpio_add "SUPERCAP_PRESENT" $GPIO_PIN1_SCL input active-low
+	wb_gpio_add "SUPERCAP_DISCHARGING"  $GPIO_PIN2_SDA input active-low
+	hook_once_after_config_change "service_restart_delete_retained wb-homa-gpio /devices/wb-gpio/#"
+}
