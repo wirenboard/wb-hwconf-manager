6b9b36553ff29f4abc44cff3d5d7f1a02e5572a3
vdromanov <vdromanov@yahoo.com>
2020-10-16 13:44:23 +0300

    fixup: identation from spaces to tabs

--- modules/wb6x-wx-di-common.sh
+++ modules/wb6x-wx-di-common.sh
@@ -6,10 +6,10 @@ hook_module_add() {
 }
 
 hook_module_del() {
-    wb_gpio_del $GPIO_DATA
+	wb_gpio_del $GPIO_DATA
 	hook_once_after_config_change "service_restart_delete_retained wb-homa-gpio /devices/wb-gpio/#"
 }
 
 hook_module_deinit() {
-    echo $GPIO_DATA > /sys/class/gpio/unexport 2>/dev/null || true
+	echo $GPIO_DATA > /sys/class/gpio/unexport 2>/dev/null || true
 }
