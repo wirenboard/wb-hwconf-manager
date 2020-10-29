41d8af6fe4a7cd06e325948ec93d1fcda73e534c
vdromanov <vdromanov@yahoo.com>
2020-10-29 19:35:55 +0300

    wbmz2-slot in wb6.7.x (#32)
    * wbmz2-slot in wb6.7.x
    
    * proper name for wbmz slot on Wiren Board 6.7
    
    * common header for wb6.x battery slot
    
    * separated common part of dts & sh script for wbmz-battery module
    
    * common json schema for wbmz2 & wbmz3 batteries
    
    * added wbmz3-supercap; common script for wbmz2/3-supercap
    
    * requires newest wb-rules-system (with renamed virtual device for wbmzX-battery)
    
    * discharge signal has inverted on wbmz3-supercap
    
    * do need for newer kernel on wb2
    
    * same dtc version as before
    
    * latest linux-image-wb2 version
    
    * use tabs instead of spaces in dtso
    
    * wbmz2-battery & wbmz3-supercap are using tab-identation instead of spaces
    
    Co-authored-by: Evgeny Boger <boger@contactless.ru>

---
+++ modules/wbmz-battery-common.sh
@@ -0,0 +1,24 @@
+source "$DATADIR/modules/utils.sh"
+
+hook_module_add() {
+	jq '.enable = true' /etc/wbmz2-battery.conf | sponge /etc/wbmz2-battery.conf
+	hook_once_after_config_change "service_restart_delete_retained wb-rules /devices/wbmz2-battery/#"
+}
+
+hook_module_del() {
+	jq '.enable = false' /etc/wbmz2-battery.conf | sponge /etc/wbmz2-battery.conf
+	hook_once_after_config_change "service_restart_delete_retained wb-rules /devices/wbmz2-battery/#"
+}
+
+hook_module_init() {
+	local bus=$(i2c_bus_num "wbmz2_i2c@0")
+	[[ -z "$bus" ]] && return 1
+	jq '.bus = '$bus /etc/wbmz2-battery.conf | sponge /etc/wbmz2-battery.conf
+	local bias_mode="$(config_module_option ".resetButon")"
+	if [ "$bias_mode" = "disabled" ]
+	then
+		jq '.resetButon = false' /etc/wbmz2-battery.conf | sponge /etc/wbmz2-battery.conf
+	else
+		jq '.resetButon = true' /etc/wbmz2-battery.conf | sponge /etc/wbmz2-battery.conf
+	fi
+}
--- modules/wbmz2-supercap.sh
+++ modules/wbmz2-supercap.sh
@@ -1,18 +1,1 @@
-source "$DATADIR/modules/utils.sh"
-
-hook_module_add() {
-	wb_gpio_add "SUPERCAP_PRESENT" $GPIO_PIN1_SCL input active-low
-	wb_gpio_add "SUPERCAP_DISCHARGING"  $GPIO_PIN2_SDA input active-high
-	hook_once_after_config_change "service_restart_delete_retained wb-homa-gpio /devices/wb-gpio/#"
-}
-
-hook_module_del() {
-	wb_gpio_del $GPIO_PIN1_SCL
-	wb_gpio_del $GPIO_PIN2_SDA
-	hook_once_after_config_change "service_restart_delete_retained wb-homa-gpio /devices/wb-gpio/#"
-}
-
-hook_module_deinit() {
-    echo $GPIO_PIN1_SCL > /sys/class/gpio/unexport 2>/dev/null || true
-    echo $GPIO_PIN2_SDA > /sys/class/gpio/unexport 2>/dev/null || true
-}
+source "$DATADIR/modules/wbmz-supercap-common.sh"
