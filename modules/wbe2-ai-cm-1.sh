912b075e7059d5e1e20a373fef95e4d12e18cf49
vdromanov <vdromanov@yahoo.com>
2021-04-05 17:12:44 +0300

    storing slot name in dt

--- modules/wbe2-ai-cm-1.sh
+++ modules/wbe2-ai-cm-1.sh
@@ -7,13 +7,3 @@ hook_module_init() {
 hook_module_deinit() {
 	rmmod ina2xx
 }
-
-hook_module_add() {
-	jq '.enable = true' /etc/wbe2-ai-cm-1.conf | sponge /etc/wbe2-ai-cm-1.conf
-	hook_once_after_config_change "service_restart_delete_retained wb-rules /devices/wbe2-ai-cm-1/#"
-}
-
-hook_module_del() {
-	jq '.enable = false' /etc/wbe2-ai-cm-1.conf | sponge /etc/wbe2-ai-cm-1.conf
-	hook_once_after_config_change "service_restart_delete_retained wb-rules /devices/wbe2-ai-cm-1/#"
-}
