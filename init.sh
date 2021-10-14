57b81036cf613546a32f4d7ee55f0d14272f39b4
Maslov Nikita <webconn@users.noreply.github.com>
2021-10-14 13:43:52 +0300

    switch to systemd service (#52)
    * switch to systemd service
    * add Homepage to debian/control
    * move scripts to /usr/lib
    * force lintian checks

---
+++ init.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+. /usr/lib/wb-hwconf-manager/functions.sh
+
+NO_RESTART_SERVICE="yes" wb-hwconf-helper config-apply
+
+grep "$CONFIGFS" /proc/mounts >/dev/null 2>&1 || {
+	echo "$CONFIGFS not mounted, skipping modules initialization"
+	return 1
+}
+
+cat "$CONFIG_STATE" | while read SLOT MODULE OPTIONS_HASH; do
+	[[ -z "$SLOT" || -z "$MODULE" ]] && continue
+	module_init "$SLOT" "$MODULE"
+done
