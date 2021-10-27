bb48019fdc93ec47011fde8c74294284a7e993f8
Petr Krasnoshchekov <petr.krasnoshchekov@wirenboard.ru>
2021-10-27 16:59:34 +0500

    wbe2-di-dr-3 port to DT

--- modules/wbe2-di-dr-3.sh
+++ modules/wbe2-di-dr-3.sh
@@ -1,9 +1,6 @@
 source "$DATADIR/modules/utils.sh"
 
 hook_module_add() {
-	wb_gpio_add "MOD${SLOT_NUM}_IN1" $GPIO_TX input active-high
-	wb_gpio_add "MOD${SLOT_NUM}_IN2" $GPIO_RX input active-high
-	wb_gpio_add "MOD${SLOT_NUM}_IN3" $GPIO_RTS input active-high
 	hook_once_after_config_change "service_restart_delete_retained wb-homa-gpio /devices/wb-gpio/#"
 }
 
@@ -12,10 +9,4 @@ hook_module_del() {
 	wb_gpio_del $GPIO_RX
 	wb_gpio_del $GPIO_RTS
 	hook_once_after_config_change "service_restart_delete_retained wb-homa-gpio /devices/wb-gpio/#"
-}
-
-hook_module_deinit() {
-    echo $GPIO_TX > /sys/class/gpio/unexport 2>/dev/null || true
-    echo $GPIO_RX > /sys/class/gpio/unexport 2>/dev/null || true
-    echo $GPIO_RTS > /sys/class/gpio/unexport 2>/dev/null || true
 }
