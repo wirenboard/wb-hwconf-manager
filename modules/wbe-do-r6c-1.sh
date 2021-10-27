a6c6adc46a7b6e959820c9da5d6d19aed85bb546
Petr Krasnoshchekov <petr.krasnoshchekov@wirenboard.ru>
2021-10-27 16:59:10 +0500

    wbe-do-r6c-1 port to DT

--- modules/wbe-do-r6c-1.sh
+++ modules/wbe-do-r6c-1.sh
@@ -1,15 +1,10 @@
 source "$DATADIR/modules/utils.sh"
 
 hook_module_add() {
-	wb_gpio_add "MOD${SLOT_NUM}_OUT1" $GPIO_CS output active-high
 	hook_once_after_config_change "service_restart_delete_retained wb-homa-gpio /devices/wb-gpio/#"
 }
 
 hook_module_del() {
 	wb_gpio_del $GPIO_CS
 	hook_once_after_config_change "service_restart_delete_retained wb-homa-gpio /devices/wb-gpio/#"
-}
-
-hook_module_deinit() {
-    echo $GPIO_CS > /sys/class/gpio/unexport 2>/dev/null || true
 }
