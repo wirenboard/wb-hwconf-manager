4d50a8c641db5dd397660753d5a6baf99640b37d
Petr Krasnoshchekov <petr.krasnoshchekov@wirenboard.ru>
2021-10-29 11:24:27 +0500

    Wrong commwnt is removed

--- slots/wb-wbe-gpio.h
+++ slots/wb-wbe-gpio.h
@@ -1,9 +1,3 @@
-
-/* order for EXTIO_INPUT and EXTIO_OUTPUT_HIGH
-   must be two digit number, 1 must be 01, 2 - 02 etc.
-   The macros concatenates EXTIO_SLOT_NUM and supplied order.
-   Resulting sort order for extio pins starts from 100.
-*/
 #define WBE_INPUT(name, pin, order) \
     __cat4(MOD, MOD_SLOT_NUM, _, name) {\
         io-gpios = <SLOT_GPIO(pin)>;\
