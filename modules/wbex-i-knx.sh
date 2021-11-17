9d8f1cae18753285e425b42dcea2a870d313363d
Nikita Maslov <nikita.maslov@wirenboard.ru>
2021-11-17 14:17:48 +0300

    wbex-i-knx: rebuild knxd config on module add/remove

--- modules/wbex-i-knx.sh
+++ modules/wbex-i-knx.sh
@@ -1,15 +1,27 @@
 source "$DATADIR/modules/utils.sh"
 
+wb_knxd_config_service_exists() {
+	systemctl --all --type service --no-legend | grep wb-knxd-config >/dev/null
+}
+
 hook_module_init() {
 	ln -s /dev/ttyMOD${SLOT_NUM} /dev/ttyKNX${SLOT_NUM}
 	if [[ ! -e /dev/ttyKNX ]]; then
 		ln -s /dev/ttyMOD${SLOT_NUM} /dev/ttyKNX	
-	fi	
+	fi
+
+	if wb_knxd_config_service_exists; then
+		systemctl restart wb-knxd-config.service
+	fi
 }
 
 hook_module_deinit() {
 	rm -f /dev/ttyKNX${SLOT_NUM}
 	if [[ $(readlink /dev/ttyKNX) == "/dev/ttyMOD${SLOT_NUM}" ]]; then
 		rm -f /dev/ttyKNX	
+	fi
+
+	if wb_knxd_config_service_exists; then
+		systemctl restart wb-knxd-config.service
 	fi
 }
