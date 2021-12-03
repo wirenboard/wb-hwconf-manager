20da5113b85728fb8f4b6ddb303440ca6cde7cfb
Evgeny Boger <boger@wirenboard.com>
2021-12-03 16:32:54 +0300

    support for sunxi xlate in GPIO definitions

--- slots/wb-wbe-gpio.h
+++ slots/wb-wbe-gpio.h
@@ -1,13 +1,17 @@
-#define WBE_INPUT(name, pin, order) \
+#define WBE_INPUT(name, pin, orderparam) \
     __cat4(MOD, MOD_SLOT_NUM, _, name) {\
         io-gpios = <SLOT_GPIO(pin)>;\
         input;\
-        sort-order = <__cat(MOD_SLOT_NUM, order)>;\
+        sort-order = <__cat(MOD_SLOT_NUM, orderparam)>;\
+        gpio-xlate-type = SLOT_GPIO_XLATE_TYPE(pin); \
+        gpio-pins-per-bank = <SLOT_GPIO_PINS_PER_BANK(pin)>; \
     }
 
-#define WBE_OUTPUT_HIGH(name, pin, order) \
+#define WBE_OUTPUT_HIGH(name, pin, orderparam) \
     __cat4(MOD, MOD_SLOT_NUM, _, name) {\
         io-gpios = <SLOT_GPIO(pin)>;\
         output-high;\
-        sort-order = <__cat(MOD_SLOT_NUM, order)>;\
+        sort-order = <__cat(MOD_SLOT_NUM, orderparam)>;\
+        gpio-xlate-type = SLOT_GPIO_XLATE_TYPE(pin); \
+        gpio-pins-per-bank = <SLOT_GPIO_PINS_PER_BANK(pin)>; \
     }
