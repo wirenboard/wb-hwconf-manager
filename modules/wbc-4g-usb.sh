7ce4c54129009a0c568f4d644ff0176338ef27f2
vdromanov <vdromanov@yahoo.com>
2023-03-29 00:38:04 +0300

    wbc-4g-usb: reveal mm-gsm-connections on modem init and hide on deinit

--- modules/wbc-4g-usb.sh
+++ modules/wbc-4g-usb.sh
@@ -1,10 +1,20 @@
 source "$DATADIR/modules/utils.sh"
 
+hide_modem_connections() {
+	[[ -f "/usr/lib/wb-nm-helper/wb-hide-connection" ]] && {
+		for conn_id in wb-gsm-sim1 wb-gsm-sim2; do
+			/usr/lib/wb-nm-helper/wb-hide-connection $conn_id "$1"
+		done
+	}
+}
+
 hook_module_add() {
+	hide_modem_connections ""
 	hook_once_after_config_change "service_restart wb-gsm"
 }
 
 hook_module_del() {
+	hide_modem_connections "true"
 	[[ -z "$NO_RESTART_SERVICE" ]] && {
 		systemctl stop wb-gsm || true
 	}
