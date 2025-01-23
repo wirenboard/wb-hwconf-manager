7d27307be7628d255d6a1b9b814f7c31a2eb4bdc
Anton Tarasov <ant0@mail.ru>
2025-01-23 11:15:06 +0300

    Added overlay for USB-hub (#136)
    * Added overlay for USB-hub
    
    * Fix the error displayed by the module.
    Add pin number detection

---
+++ modules/wbc-usb-hub.sh
@@ -0,0 +1,14 @@
+source "$DATADIR/modules/utils.sh"
+
+GPIO_USB_POWER=$(gpiofind "GSM ON" 2>/dev/null | awk '{print $2}') || GPIO_USB_POWER=132
+
+hook_module_init() {
+    systemctl stop wb-gsm || true
+
+    sysfs_gpio_export $GPIO_USB_POWER
+    sysfs_gpio_set $GPIO_USB_POWER 1
+}
+
+hook_module_deinit() {
+    sysfs_gpio_unexport $GPIO_USB_POWER
+}
