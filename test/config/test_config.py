d12a06f422cff171be4292e073eabda123252fb3
Ekaterina Volkova <ekaterina.volkova@wirenboard.com>
2025-02-17 22:05:56 +0300

    fix tests

--- test/config/test_config.py
+++ test/config/test_config.py
@@ -7,12 +7,13 @@ OLD_CONFIG_PATH = "./test/config/old_config.conf"
 NEW_CONFIG_PATH = "./test/config/new_config.conf"
 BOARD_CONF_PATH = "./test/config/system.conf"
 CONFED_JSON_PATH = "./test/config/confed.json"
+VENDOR_CONFIG_PATH = "./test/vendor-modules.json"
 
 
 def test_make_modules_list():
     with open(CONFED_JSON_PATH, "r", encoding="utf-8") as config_file:
         confed_json = json.load(config_file)
-    assert confed_json["modules"] == make_modules_list(MODULES_DIR)
+    assert confed_json["modules"] == make_modules_list(MODULES_DIR, VENDOR_CONFIG_PATH)
 
 
 def test_to_confed_from_old_config():
@@ -24,7 +25,7 @@ def test_to_confed_from_old_config():
     with open(CONFED_JSON_PATH, "r", encoding="utf-8") as config_file:
         confed_json = json.load(config_file)
     for config_path in config_paths:
-        assert confed_json == to_confed(config_path, BOARD_CONF_PATH, MODULES_DIR)
+        assert confed_json == to_confed(config_path, BOARD_CONF_PATH, MODULES_DIR, VENDOR_CONFIG_PATH)
 
 
 def test_to_confed_from_new_config():
@@ -36,7 +37,7 @@ def test_to_confed_from_new_config():
     with open(CONFED_JSON_PATH, "r", encoding="utf-8") as config_file:
         confed_json = json.load(config_file)
     for config_path in config_paths:
-        assert confed_json == to_confed(config_path, BOARD_CONF_PATH, MODULES_DIR)
+        assert confed_json == to_confed(config_path, BOARD_CONF_PATH, MODULES_DIR, VENDOR_CONFIG_PATH)
 
 
 def test_from_confed():
@@ -44,4 +45,4 @@ def test_from_confed():
         confed_str = confed_file.read()
     with open(NEW_CONFIG_PATH, "r", encoding="utf-8") as config_file:
         new_config = json.load(config_file)
-    assert new_config == from_confed(confed_str, BOARD_CONF_PATH, MODULES_DIR)
+    assert new_config == from_confed(confed_str, BOARD_CONF_PATH, MODULES_DIR, VENDOR_CONFIG_PATH)
