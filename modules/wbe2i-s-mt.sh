6be463da8a6b71c2879a18e6124a6b0fc54d2d09
vdromanov <vdromanov@yahoo.com>
2025-03-13 14:04:20 +0300

    slight review changes

--- modules/wbe2i-s-mt.sh
+++ modules/wbe2i-s-mt.sh
@@ -2,11 +2,11 @@ source "$DATADIR/modules/utils.sh"
 
 hook_module_init() {
 	bus_num=$(readlink -f /dev/i2c-mod${SLOT_NUM} | sed -r 's/^\/dev\/i2c-//')
-	echo "{'bus_num': $bus_num}" > /etc/wb-mqtt-tlv493.conf
+	echo "{'bus_num': $bus_num}" > /var/lib/wb-mqtt-tlv493/conf.d/wb-mqtt-tlv493.conf
 	systemctl restart wb-mqtt-tlv493
 }
 
 hook_module_deinit() {
-	rm /etc/wb-mqtt-tlv493.conf
+	rm /var/lib/wb-mqtt-tlv493/conf.d/wb-mqtt-tlv493.conf
 	systemctl restart wb-mqtt-tlv493
 }
