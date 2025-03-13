4e6e120658f70f99f57f43e509290cb86e86c3f9
vdromanov <vdromanov@yahoo.com>
2025-03-13 19:21:06 +0300

    finally fix stuck on boot

--- modules/wbe2i-s-mt.sh
+++ modules/wbe2i-s-mt.sh
@@ -1,12 +1,13 @@
 source "$DATADIR/modules/utils.sh"
 
-hook_module_init() {
+hook_module_add() {
 	bus_num=$(readlink -f /dev/i2c-mod${SLOT_NUM} | sed -r 's/^\/dev\/i2c-//')
+	mkdir -p /var/lib/wb-mqtt-tlv493/conf.d/
 	echo "{'bus_num': $bus_num}" > /var/lib/wb-mqtt-tlv493/conf.d/wb-mqtt-tlv493.conf
 	systemctl restart wb-mqtt-tlv493 || true
 }
 
-hook_module_deinit() {
-	rm /var/lib/wb-mqtt-tlv493/conf.d/wb-mqtt-tlv493.conf
+hook_module_del() {
+	rm /var/lib/wb-mqtt-tlv493/conf.d/wb-mqtt-tlv493.conf || true
 	systemctl restart wb-mqtt-tlv493 || true
 }
