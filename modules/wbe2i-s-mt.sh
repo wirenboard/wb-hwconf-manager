c678814ccacda42bbc08e208230b7ca01d126d83
vdromanov <vdromanov@yahoo.com>
2025-03-16 21:10:48 +0300

    finally fix boot stuck

--- modules/wbe2i-s-mt.sh
+++ modules/wbe2i-s-mt.sh
@@ -1,13 +1,17 @@
 source "$DATADIR/modules/utils.sh"
 
-hook_module_add() {
+service_name="wb-mqtt-tlv493"
+
+hook_module_init() {
+	systemctl is-active wb-hwconf-manager && systemctl stop $service_name || true
 	bus_num=$(readlink -f /dev/i2c-mod${SLOT_NUM} | sed -r 's/^\/dev\/i2c-//')
-	mkdir -p /var/lib/wb-mqtt-tlv493/conf.d/
-	echo "{'bus_num': $bus_num}" > /var/lib/wb-mqtt-tlv493/conf.d/wb-mqtt-tlv493.conf
-	systemctl restart wb-mqtt-tlv493 || true
+	mkdir -p "/var/lib/${service_name}/conf.d/"
+	echo "{'bus_num': $bus_num}" > "/var/lib/${service_name}/conf.d/wb-mqtt-tlv493.conf"
+	systemctl is-active wb-hwconf-manager && systemctl restart $service_name || true
 }
 
-hook_module_del() {
+hook_module_deinit() {
+	systemctl is-active wb-hwconf-manager && systemctl stop $service_name || true
 	rm /var/lib/wb-mqtt-tlv493/conf.d/wb-mqtt-tlv493.conf || true
-	systemctl restart wb-mqtt-tlv493 || true
+	systemctl is-active wb-hwconf-manager && systemctl restart $service_name || true
 }
