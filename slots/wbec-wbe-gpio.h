46719c1aa7ef0058b5751e589c29ae980e6ac149
Anton Tarasov <mail@taraant.me>
2026-01-20 23:06:19 +0300

    wb85: split WBEC pin defs and simplify slot includes

--- slots/wbec-wbe-gpio.h
+++ slots/wbec-wbe-gpio.h
@@ -1,11 +1,3 @@
-#undef __gpio_pin
-#define __gpio_pin __arg1
-
-#undef SLOT_GPIO_PORT_ALIAS
-#define SLOT_GPIO_PORT_ALIAS(x) &wbec_gpio
-#undef SLOT_GPIO_SPEC
-#define SLOT_GPIO_SPEC(x) SLOT_GPIO_PIN(x)
-
 #undef WBE_INPUT
 #define WBE_INPUT(name, pin, index) \
     __cat4(MOD, MOD_SLOT_NUM, _, name) {\
@@ -20,12 +12,3 @@
         io-gpios = <SLOT_GPIO(pin)>;\
         sort-order = <__cat(MOD_SLOT_NUM, index)>;\
     }
-
-#ifdef FROM_SHELL
-#ifdef SLOT_ALL_PINS
-#undef SLOT_FOR_PIN
-#define SLOT_FOR_PIN(x) local GPIO_##x=SLOT_GPIO_PIN(x);
-SLOT_ALL_PINS
-#undef SLOT_FOR_PIN
-#endif
-#endif
